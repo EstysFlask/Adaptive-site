a2cff2d3ffbd8fab38fe0cf4e758164581c86c93
Dmitry Fedotov <estysflask@gmail.com>
2018-09-22 13:09:17 +0300

    Add new projects

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ menu.onclick = function menuClick() {
     myNav.className = "topnav";
   }
 }
+
+
+function snack() {
+  var bar = document.getElementById("snackbar");
+  bar.className = "show";
+  setTimeout(function(){
+    bar.classList.remove("show");
+  }, 3000);
+}
